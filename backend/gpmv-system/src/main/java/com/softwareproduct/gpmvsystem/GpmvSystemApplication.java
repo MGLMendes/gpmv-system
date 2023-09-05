e8becc194a8197b6eea61d3da18bc7fff44fae77
MGLMendes <miguelmendes203@gmail.com>
2023-09-05 19:53:15 -0300

    Projeto Iniciado

---
+++ backend/gpmv-system/src/main/java/com/softwareproduct/gpmvsystem/GpmvSystemApplication.java
@@ -0,0 +1,13 @@
+package com.softwareproduct.gpmvsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpmvSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpmvSystemApplication.class, args);
+	}
+
+}
---
+++ backend/gpmv-system/src/test/java/com/softwareproduct/gpmvsystem/GpmvSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareproduct.gpmvsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpmvSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
