a8d387a53cdd9b9e7837dac2333607bcc998f041
MGLMendes <miguelmendes203@gmail.com>
2023-10-24 21:11:13 -0300

    front de beneficios

--- backend/gpmv-system/src/main/java/com/softwareproduct/gpmvsystem/domain/repository/BeneficioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.softwareproduct.gpmvsystem.domain.repository;
-
-import com.softwareproduct.gpmvsystem.domain.model.Beneficio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BeneficioRepository extends JpaRepository<Beneficio, Long> {
-}
